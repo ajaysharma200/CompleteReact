f4ea203f39e9b0656aafefd927a568262e7e8465
ajaysharma200 <sharmaajay212003@gmail.com>
2025-07-29 19:02:15 +0530

    Movie Web Page Using Bootstrap & Basic React

---
+++ MovieZone/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import MovieZone from './components/Movies'
+
+const App = () => {
+  return (
+    <div>
+      
+      <MovieZone />
+    </div>
+  )
+}
+
+export default App
